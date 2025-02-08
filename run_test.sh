672268879135d1b51397c17e29e4a4f9faeaaca3
Juan E. Arango <juanes.ao@gmail.com>
2025-02-07 22:23:31 -0500

    ✅ keep generic test case

--- run_test.sh
+++ run_test.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
 
-nextflow run main.nf \
-    -profile hpc_slurm \
-    --step preprocess \
+ nextflow run main.nf \
+    --step full \
     --vcf tests/data/vcf/test_snv.vcf \
     --bam tests/data/tumor/tumor.bam \
-    --reference tests/data/reference/reference.fasta
-
-
-
-nextflow run main.nf \
-    --step preprocess \
-    --vcf /data1/papaemme/isabl/data/analyses/32/64/513264/any2lcc.vcf \
-    --bam /data1/papaemme/isabl/data/analyses/56/81/425681/IID_H203539_T02_01_WG01.bam \
-    --reference /data1/papaemme/isabl/ref/homo_sapiens/GRCh37d5/genome/gr37.fasta \
-    --model /data1/papaemme/isabl/ref/homo_sapiens/GRCh37d5/formalinfixer/indel_model.pkl \
-    --modelName ARTIFACT \
-    --outdir results2 \
-    --coverage 81 \
-    --medianInsert 349
-
-
-nextflow run main.nf \
-    --step full \
-    --vcf /data1/papaemme/isabl/data/analyses/32/64/513264/any2lcc.vcf \
-    --bam /data1/papaemme/isabl/data/analyses/56/81/425681/IID_H203539_T02_01_WG01.bam \
-    --reference /data1/papaemme/isabl/ref/homo_sapiens/GRCh37d5/genome/gr37.fasta \
-    --model /data1/papaemme/isabl/ref/homo_sapiens/GRCh37d5/formalinfixer/indel_model.pkl \
-    --modelName ARTIFACT \
-    --outdir results2 \
-    --coverage 81 \
-    --medianInsert 349 \
-    --mutationType indels
+    --reference tests/data/reference/reference.fasta \
+    --bed tests/data/reference/reference.bed \
+    --outdir results \
+    --coverage 76 \
+    --medianInsert 254 \
+    --model tests/data/models/snv_model.pkl \
+    --modelName ARTIFACT
