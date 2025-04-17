a0a6f20ec359d4b9b15b0857b0988b4ad3d0f063
Juan E. Arango Ossa <juanes.ao@gmail.com>
2025-04-17 16:54:01 -0400

    🔧 replace pickle for joblib and add test profile

--- bin/classify_w_random_forest.py
+++ bin/classify_w_random_forest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import pickle
+import joblib
 from pathlib import Path
 import pandas as pd
 
@@ -14,7 +14,7 @@ def classify_with_random_forest(
 
     Args:
         features (str): Path to tsv with preprocessed features.
-        model (str): Path to the trained model (pickle file).
+        model (str): Path to the trained model (joblib file).
         model_name (str): Name of the model for labeling outputs.
         mutation_type (str): Type of mutation ("snvs" or "indels").
         annotated_tsv (str, optional): Path to tsv file to add annotation.
@@ -32,10 +32,10 @@ def classify_with_random_forest(
 
     # Load and validate model
     with open(model_path, "rb") as model_file:
-        model = pickle.load(model_file)
+        model = joblib.load(model_file)
 
     if not hasattr(model, "predict") or not hasattr(model, "predict_proba"):
-        raise Exception("Invalid pickle file model: Missing necessary methods.")
+        raise Exception("Invalid joblib file model: Missing necessary methods.")
     
     # Load input dataframe
     features_df = pd.read_csv(features_path, sep="\t", low_memory=False)
@@ -96,7 +96,7 @@ if __name__ == "__main__":
         help="Path to tsv with preprocessed features.",
     )
     parser.add_argument(
-        "--model", required=True, help="Path to the trained model (pickle file)."
+        "--model", required=True, help="Path to the trained model (joblib file)."
     )
     parser.add_argument(
         "--model-name", required=True, help="Name of the model for labeling outputs."
